ee3a39829be83ab289ecf34fd91c82ce13c3f7c9
Mauricio Edson de Souza <mauricioesdonsouza@gmail.com>
2022-03-03 16:08:33 -0300

    Segunda aula sobre condição e ternario

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,7 +11,6 @@ function Page() {
 
   //RENDIRAZAÇÃO CONDICIONAL OU CONDIÇÃO TERNARIA
 
-  
   function initialGA() {
     ReactGA.initialize("UA-170909874-1");
     ReactGA.pageview(urlLocal);
@@ -120,13 +119,14 @@ function Page() {
 
 
   // null | "" | undefined | false
-const existe = true
 
+  const existe = true
+  const ter = true
+  const ativo = true
+  // 1 se eu colocar minha variavel com um valor que existe meu select vai exibir os cultos de sabados
+  //quando nao atender ela vai exibir cultos de terça feira
 
-// 1 se eu colocar minha variavel com um valor que existe meu select vai exibir os cultos de sabados
-//quando nao atender ela vai exibir cultos de terça feira
-
-// 2 criar uma segunda variavel que se for igual a "Ativo" exiba o botao inscrever
+  // 2 criar uma segunda variavel que se for igual a "Ativo" exiba o botao inscrever
 
   return (
     <>
@@ -136,115 +136,124 @@ const existe = true
         <title>Igreja Batista Água Viva - Ribeirão Pires</title>
       </Head>
       {existe && (
-      <div className="container">
-        <div className="header">
-          <img src="/images/logo.jpg" />
-          <h1>Formulário para culto dos jovens (24/04)</h1>
-        </div>
+        <div className="container">
+          <div className="header">
+            <img src="/images/logo.jpg" />
+            <h1>Formulário para culto dos jovens (24/04)</h1>
+          </div>
 
-        {/* {(qtdSabado < qtdMAxima || */}
-        {(qtdDomingoManha < qtdMAxima || qtdDomingoNoite < qtdMAxima) && (
-          // <>
-          //   {(dia_sem == 0 || dia_sem == 5 || dia_sem == 6) && (
-              <>
-                {listagem ? (
-                  <div className="form">
-                    <label>Nome</label>
-                    {nomeLimpar && (
-                      <input
-                        onKeyUp={(e) => setNome(e.currentTarget.value)}
-                        type="text"
-                        placeholder="Nome "
-                        className="input_name"
-                        required
-                      />
-                    )}
-                    {!nomeLimpar && (
-                      <input
-                        onKeyUp={(e) => setNome(e.currentTarget.value)}
-                        type="text"
-                        placeholder="Nome "
-                        className="input_name"
-                        required
-                      />
-                    )}
+          {/* {(qtdSabado < qtdMAxima || */}
+          {(qtdDomingoManha < qtdMAxima || qtdDomingoNoite < qtdMAxima) && (
+            // <>
+            //   {(dia_sem == 0 || dia_sem == 5 || dia_sem == 6) && (
+            <>
+              {listagem ? (
+                <div className="form">
+                  <label>Nome</label>
+                  {nomeLimpar && (
+                    <input
+                      onKeyUp={(e) => setNome(e.currentTarget.value)}
+                      type="text"
+                      placeholder="Nome "
+                      className="input_name"
+                      required
+                    />
+                  )}
+                  {!nomeLimpar && (
+                    <input
+                      onKeyUp={(e) => setNome(e.currentTarget.value)}
+                      type="text"
+                      placeholder="Nome "
+                      className="input_name"
+                      required
+                    />
+                  )}
 
-                    <br></br>
-                    <label>Sobrenome</label>
-                    {sobrenomeLimpar && (
-                      <input
-                        onKeyUp={(e) => setSobrenome(e.currentTarget.value)}
-                        type="text"
-                        placeholder="Sobrenome"
-                        required
-                      />
-                    )}
-                    {!sobrenomeLimpar && (
-                      <input
-                        onKeyUp={(e) => setSobrenome(e.currentTarget.value)}
-                        type="text"
-                        placeholder="Sobrenome"
-                        required
-                      />
-                    )}
-                    <br></br>
-                    <label>Discipulado</label>
-                    {redeLimpar && (
-                      <select
-                        onChange={(e) => {
-                          setRede(e.target.value);
-                        }}
-                        required
-                      >
-                        <option value="">Selecione seu discipulado</option>
-                        <option value="isabela">Isabela Amaro</option>
-                        <option value="matheus">Matheus Amaro</option>
-                        <option value="felipe">Felipe Ramos</option>
-                        <option value="silas">
-                          Silas Souza
-                        </option>
-                        <option value="elaine">Elaine Jeremias </option>
-                      </select>
-                    )}
-                    {!redeLimpar && (
-                      <select
-                        onChange={(e) => {
-                          setRede(e.target.value);
-                        }}
-                        required
-                      >
-                        <option value="">Selecione seu discipulado</option>
-                        <option value="isabela">Isabela Amaro</option>
-                        <option value="matheus">Matheus Amaro</option>
-                        <option value="felipe">Felipe Ramos</option>
-                        <option value="silas">
-                          Silas Souza
-                        </option>
-                        <option value="elaine">Elaine Jeremias </option>
-                      </select>
-                    )}
-                    <br></br>
-                    <label>Qual culto deseja ir?</label>
+                  <br></br>
+                  <label>Sobrenome</label>
+                  {sobrenomeLimpar && (
+                    <input
+                      onKeyUp={(e) => setSobrenome(e.currentTarget.value)}
+                      type="text"
+                      placeholder="Sobrenome"
+                      required
+                    />
+                  )}
+                  {!sobrenomeLimpar && (
+                    <input
+                      onKeyUp={(e) => setSobrenome(e.currentTarget.value)}
+                      type="text"
+                      placeholder="Sobrenome"
+                      required
+                    />
+                  )}
+                  <br></br>
+                  <label>Discipulado</label>
+                  {redeLimpar && (
                     <select
                       onChange={(e) => {
-                        setCulto(e.target.value);
+                        setRede(e.target.value);
                       }}
                       required
                     >
-                      <option value="">Selecione o culto</option>
-                      {/* {qtdSabado < qtdMAxima && (
-                        <option value="sabado">Sábado - 19hs</option>
-                      )} */}
-                      {qtdDomingoManha < qtdMAxima && (
-                        <option value="manha">Sábado - 15hs</option>
-                      )}
-                      {qtdDomingoNoite < qtdMAxima && (
-                        <option value="noite">Sábado - 17:30hs </option>
-                      )}
+                      <option value="">Selecione seu discipulado</option>
+                      <option value="isabela">Isabela Amaro</option>
+                      <option value="matheus">Matheus Amaro</option>
+                      <option value="felipe">Felipe Ramos</option>
+                      <option value="silas">
+                        Silas Souza
+                      </option>
+                      <option value="elaine">Elaine Jeremias </option>
                     </select>
-                    {/* <br></br>
+                  )}
+                  {!redeLimpar && (
+                    <select
+                      onChange={(e) => {
+                        setRede(e.target.value);
+                      }}
+
+                      required
+                    >
+
+                      <option value="">Selecione seu discipulado</option>
+                      <option value="isabela">Isabela Amaro</option>
+                      <option value="matheus">Matheus Amaro</option>
+                      <option value="felipe">Felipe Ramos</option>
+                      <option value="silas">
+                        Silas Souza
+                      </option>
+                      <option value="elaine">Elaine Jeremias </option>
+                    </select>
+                  )}
+                  <br></br>
+                  <label>Qual culto deseja ir?</label>
+                  <select
+                    onChange={(e) => {
+                      setCulto(e.target.value);
+                    }
+                    }
+                    required
+                  >
+                    <option value="">Selecione o culto</option>
+                    {ter ? (
+                      <>
+                        {qtdSabado < qtdMAxima && (
+                          <option value="sabado">Sábado - 19hs</option>
+                        )}
+                        {qtdDomingoManha < qtdMAxima && (
+                          <option value="manha">Sábado - 15hs</option>
+                        )}
+                        {qtdDomingoNoite < qtdMAxima && (
+                          <option value="noite">Sábado - 17:30hs </option>
+                        )}
+                      </>
+                    ) : (
+                      <option value="noite">Terça - 17:30hs </option>
+                    )}
+                  </select>
+                  {/* <br></br>
                     <label>Quantas crianças vai levar para o cultinho?</label> */}
-                    {/* {criancasLimpar && (
+                  {/* {criancasLimpar && (
                       <input
                         onKeyUp={(e) => setCriancas(e.currentTarget.value)}
                         type="number"
@@ -260,8 +269,8 @@ const existe = true
                         required
                       />
                     )} */}
-                    <div className="vagas">
-                      {/* <span>
+                  <div className="vagas">
+                    {/* <span>
                         Sábado 19hs -
                         {qtdSabado >= qtdMAxima && (
                           <small className="red"> vagas encerradas</small>
@@ -280,107 +289,111 @@ const existe = true
                           </>
                         )}
                       </span> */}
-                      <span>
-                        Sábado 15:00hs -
-                        {qtdDomingoManha >= qtdMAxima && (
-                          <small className="red"> vagas encerradas</small>
-                        )}
-                        {qtdDomingoManha < qtdMAxima && (
-                          <>
-                            {qtdDomingoManha >= 0 && (
-                              <>
-                                {" "}
-                                resta
-                                {qtdDomingoManha != qtdMAxima - 1 && <>m</>}
-                              </>
-                            )}
-                            {qtdDomingoManha >= 0 && (
-                              <small> {qtdMAxima - qtdDomingoManha}</small>
-                            )}
-                            {qtdDomingoManha >= 0 && (
-                              <>
-                                {" "}
-                                vaga{qtdDomingoManha != qtdMAxima - 1 && <>s</>}
-                              </>
-                            )}
-                          </>
-                        )}
-                      </span>
-                      <span>
-                        Sábado 17:30hs -
-                        {qtdDomingoNoite >= qtdMAxima && (
-                          <small className="red"> vagas encerradas</small>
-                        )}
-                        {qtdDomingoNoite < qtdMAxima && (
-                          <>
-                            {qtdDomingoNoite >= 0 && (
-                              <>
-                                {" "}
-                                resta
-                                {qtdDomingoNoite != qtdMAxima - 1 && <>m</>}
-                              </>
-                            )}
-                            {qtdDomingoNoite >= 0 && (
-                              <small> {qtdMAxima - qtdDomingoNoite}</small>
-                            )}
-                            {qtdDomingoNoite >= 0 && (
-                              <>
-                                {" "}
-                                vaga{qtdDomingoNoite != qtdMAxima - 1 && <>s</>}
-                              </>
-                            )}
-                          </>
-                        )}
-                      </span>
-                    </div>
-
-                    <br></br>
-                    <button onClick={salve}>Inscrever</button>
-                    <a href="/listagem.html" className="listagem">
-                      Verifique a listagem
-                    </a>
+                    <span>
+                      {/* {ter ? <>Sábado 15:00hs - Sabado 19:00hs - </> : <>terça-feira 19:00hs</>} */}
+                      {qtdDomingoManha >= qtdMAxima && (
+                        <small className="red"> vagas encerradas</small>
+                      )}
+                      {qtdDomingoManha < qtdMAxima && (
+                        <>
+                          {qtdDomingoManha >= 0 && (
+                            <>
+                              {" "}
+                              resta
+                              {qtdDomingoManha != qtdMAxima - 1 && <>m</>}
+                            </>
+                          )}
+                          {qtdDomingoManha >= 0 && (
+                            <small> {qtdMAxima - qtdDomingoManha}</small>
+                          )}
+                          {qtdDomingoManha >= 0 && (
+                            <>
+                              {" "}
+                              vaga{qtdDomingoManha != qtdMAxima - 1 && <>s</>}
+                            </>
+                          )}
+                        </>
+                      )}
+                    </span>
+                    <span>
+                      Sábado 17:30hs -
+                      {qtdDomingoNoite >= qtdMAxima && (
+                        <small className="red"> vagas encerradas</small>
+                      )}
+                      {qtdDomingoNoite < qtdMAxima && (
+                        <>
+                          {qtdDomingoNoite >= 0 && (
+                            <>
+                              {" "}
+                              resta
+                              {qtdDomingoNoite != qtdMAxima - 1 && <>m</>}
+                            </>
+                          )}
+                          {qtdDomingoNoite >= 0 && (
+                            <small> {qtdMAxima - qtdDomingoNoite}</small>
+                          )}
+                          {qtdDomingoNoite >= 0 && (
+                            <>
+                              {" "}
+                              vaga{qtdDomingoNoite != qtdMAxima - 1 && <>s</>}
+                            </>
+                          )}
+                        </>
+                      )}
+                    </span>
                   </div>
-                ) : (
-                  <p className="carregando">
-                    <img src="/images/loading.gif" />
-                    <span>Carregando...</span>
-                  </p>
-                )}
-              </>
-          //   )}
-          // </>
-        )}
-
-        {/* {qtdSabado >= qtdMAxima && */}
-        {qtdDomingoManha >= qtdMAxima &&
-          qtdDomingoNoite >= qtdMAxima &&
-          dia_sem != 1 &&
-          dia_sem != 2 &&
-          dia_sem != 3 &&
-          dia_sem != 4 && (
-            <div>
-              <p className="paragrafo">
-                Infelizmente todos os cultos presenciais ja esgotaram sua
-                capacidade exigida por lei, mas acompanhe online pelas nossas
-                redes sociais
-                <div className="redes">
-                  <a href="https://www.youtube.com/channel/UCSgeLNbDf68linYZ7XBzuug/videos">
-                    <img src="/images/youtube.svg" className="icon" />
-                  </a>
-                  <a href="https://www.facebook.com/IbavRibeiraoPires/">
-                    <img src="/images/facebook.svg" className="icon" />
-                  </a>
-                  <a href="https://www.instagram.com/ibavrp/">
-                    <img src="/images/instagram.svg" className="icon" />
+                  
+                  <br></br>
+                
+                  {!ativo && 
+                    <button onClick={salve}>Inscrever</button>
+                  }                
+                                    
+                  <a href="/listagem.html" className="listagem">
+                    Verifique a listagem
                   </a>
                 </div>
-                <a href="/listagem.html" className="listagem_encerrada">
-                  Verifique a listagem dos cultos presenciais
-                </a>
-              </p>
-            </div>
+              ) : (
+                <p className="carregando">
+                  <img src="/images/loading.gif" />
+                  <span>Carregando...</span>
+                </p>
+              )}
+            </>
+            //   )}
+            // </>
           )}
-        {/* {dia_sem >= 1 && dia_sem <= 4 && (
+
+          {/* {qtdSabado >= qtdMAxima && */}
+          {qtdDomingoManha >= qtdMAxima &&
+            qtdDomingoNoite >= qtdMAxima &&
+            dia_sem != 1 &&
+            dia_sem != 2 &&
+            dia_sem != 3 &&
+            dia_sem != 4 && (
+              <div>
+                <p className="paragrafo">
+                  Infelizmente todos os cultos presenciais ja esgotaram sua
+                  capacidade exigida por lei, mas acompanhe online pelas nossas
+                  redes sociais
+                  <div className="redes">
+                    <a href="https://www.youtube.com/channel/UCSgeLNbDf68linYZ7XBzuug/videos">
+                      <img src="/images/youtube.svg" className="icon" />
+                    </a>
+                    <a href="https://www.facebook.com/IbavRibeiraoPires/">
+                      <img src="/images/facebook.svg" className="icon" />
+                    </a>
+                    <a href="https://www.instagram.com/ibavrp/">
+                      <img src="/images/instagram.svg" className="icon" />
+                    </a>
+                  </div>
+                  <a href="/listagem.html" className="listagem_encerrada">
+                    Verifique a listagem dos cultos presenciais
+                  </a>
+                </p>
+              </div>
+            )}
+          {/* {dia_sem >= 1 && dia_sem <= 4 && (
           <div>
             <p className="paragrafo">
               O site só abrirá as inscrições <b>sexta-feira as 00hs</b>
@@ -399,8 +412,8 @@ const existe = true
             </p>
           </div>
         )} */}
-      </div>
-    )}
+        </div>
+      )}
     </>
   );
 }
